f0c06b50ccbda24aed44ba33020fb8872d566007
pfremont <46523274+PaulFremont3@users.noreply.github.com>
2026-04-01 17:35:48 -0400

    Delete Post_processing/run_comparisons_3D_analysis.sh

--- Post_processing/run_comparisons_3D_analysis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-Rscript comparisons_3D_analysis.R log10 mol int
-Rscript comparisons_3D_analysis.R log10 ind int
-Rscript comparisons_3D_analysis.R 0 ind int
-Rscript comparisons_3D_analysis.R 0 mol int
