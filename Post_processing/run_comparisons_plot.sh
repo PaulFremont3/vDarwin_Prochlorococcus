f3d09e6f5733942a99905d74be1e4c6d93e6caf4
pfremont <46523274+PaulFremont3@users.noreply.github.com>
2026-04-01 17:36:24 -0400

    Update run_comparisons_plot.sh

--- Post_processing/run_comparisons_plot.sh
+++ Post_processing/run_comparisons_plot.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 types="mol ind"
 scales="0 log10"
-#refs="all_no_virus all_virus no_virus virus virus_shuttle no_virus_shuttle"
-#refs="virus_shuttle no_virus_shuttle"
-#refs="virus no_virus no_virus_shunt virus_shunt"
-refs="virus no_virus no_virus_shunt virus_shunt no_virus_shunt_all virus_shunt_all no_virus_misc virus_misc virus_I no_virus_I"
-#refs="no_virus_shunt no_virus_shunt_all"
+
+refs="virus no_virus no_virus_shunt virus_shunt no_virus_shunt_all virus_shunt_all no_virus_misc virus_misc no_virus_I virus_I"
+
 for t in $types;
 do
 	for s in $scales;
