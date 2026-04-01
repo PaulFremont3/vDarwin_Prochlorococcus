08355612cb35e0573be849ef53d0c7a160367e6f
pfremont <46523274+PaulFremont3@users.noreply.github.com>
2026-04-01 17:36:06 -0400

    Update run_comparisons_save.sh

--- Post_processing/run_comparisons_save.sh
+++ Post_processing/run_comparisons_save.sh
@@ -3,8 +3,6 @@ types="mol ind"
 scales="0 log10"
 
 refs="virus no_virus no_virus_shunt virus_shunt no_virus_shunt_all virus_shunt_all no_virus_misc virus_misc no_virus_I virus_I"
-#refs="no_virus_shunt_all"
-#refs="no_virus_I virus_I"
 
 for t in $types;
 do
